e033a5181f8750f017b8ac21c26ca202492e01e3
Daffabot <daffabot@programmer.net>
2024-04-27 10:14:59 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,34 +11,6 @@ export default {
   components: {
     HomePage,
   },
-  mounted() {
-    this.$nextTick(() => {
-      window.addEventListener("scroll", this.setScrollVar);
-      window.addEventListener("resize", this.setScrollVar);
-      this.setScrollVar(); // Call after DOM is ready
-    });
-  },
-  methods: {
-    setScrollVar() {
-      const htmlElement = document.getElementById("top");
-      if (htmlElement) { // Check if element exists
-        const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
-        const au = percentOfScreenHeightScrolled * 100;
-        console.log("hasil: " + au);
-        htmlElement.style.setProperty("--scroll", au);
-      } else {
-        console.warn("document.body not yet available");
-      }
-    },
-    handleAnimationEnd() {
-      if (this.$refs.animated && this.$refs.anime) {
-        this.$refs.animated.removeAttribute("id");
-        this.$refs.anime.removeAttribute("id");
-      } else {
-        console.warn("Animated elements not found");
-      }
-    },
-  },
 };
 </script>
 
