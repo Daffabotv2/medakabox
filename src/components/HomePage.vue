386a70e3c5b92d4fcedc061457ccfbca84f6a132
Daffabot <daffabot@programmer.net>
2024-04-27 08:16:14 +0700

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -90,7 +90,7 @@ export default {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
     :root {
       --color-one: 18, 55, 42;
       --color-two: 67, 104, 80;
