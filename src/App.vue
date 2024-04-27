ed53709ecd1e47e3d61cb8a08ed7f9855e73d39c
Daffabot <daffabot@programmer.net>
2024-04-27 07:24:40 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <HomePage />
+  </div>
 </template>
 
 <script>
@@ -9,6 +11,27 @@ export default {
   name: 'App',
   components: {
     HomePage
+  },
+  mounted() {
+    window.addEventListener("scroll", this.setScrollVar);
+    window.addEventListener("resize", this.setScrollVar);
+    this.setScrollVar(); // Panggil fungsi saat komponen dimuat pertama kali
+  },
+  methods: {
+    setScrollVar() {
+        const htmlElement = document.body;
+        const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
+        const au = percentOfScreenHeightScrolled * 100;
+        console.log("hasil: " + au);
+        htmlElement.style.setProperty(
+          "--scroll",
+          au
+        );
+      }
+    handleAnimationEnd() {
+      this.$refs.animated.removeAttribute("id");
+      this.$refs.anime.removeAttribute("id");
+    }
   }
 }
 </script>
