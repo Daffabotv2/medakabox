32bf0fff60621fa14b4d46e8a16294a50c87d9f1
DAFFA AHMAD IBRAHIM <124247480+Daffabotv2@users.noreply.github.com>
2024-05-21 14:23:59 +0000

    apalah

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomePage from './views/HomePage.vue';
-import AboutPage from './views/AboutPage.vue';
+import HomePage from './views/Home.vue';
+import AboutPage from './views/About.vue';
 
 Vue.use(VueRouter);
 
