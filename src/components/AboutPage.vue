4967fa1ae11e3e9fff43337001e1a1457e27f0e5
Daffabot <daffabot@programmer.net>
2024-05-21 13:44:39 +0000

    bisalah

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -6,7 +6,7 @@
       <ul class="flex sm:flex-row-reverse max-[640px]:flex-col justify-center items-center text-center transition duration-500 ease-in-out"
         :class="{'max-[640px]:hidden': !open }">
         <li>
-          <h1 class="text-color-two max-[640px]:mx-auto sm:mr-8 max-[330px]:text-4xl max-[640px]:text-6xl font-bold max-[640px]:mb-20 transition duration-500" :class="{'nav-ani': open }">>Home</h1>
+          <h1 class="text-color-two max-[640px]:mx-auto sm:mr-8 max-[330px]:text-4xl max-[640px]:text-6xl font-bold max-[640px]:mb-20 transition duration-500" :class="{'nav-ani': open }"><router-link to="/">>Home</router-link></h1>
         </li>
         <li class="text-color-two sm:mr-8 transition duration-500" :class="{'nav-ani': open }">Social: <a href="https://twitter.com/daffabot_id" target="_blank" rel="noopener noreferrer" class="underline">TW</a> <a href="https://github.com/Daffabot" target="_blank" rel="noopener noreferrer" class="underline">GH</a> <a href="https://linkedin.com/in/daffabot" target="_blank" rel="noopener noreferrer" class="underline">LI</a></li>
         <li class="text-color-two sm:mr-8 transition duration-500" :class="{'nav-ani': open }">Available For Exigent <a href="https://wa.me/+6285156207150?text=Halo+Bang" rel="noopener noreferrer" target="_blank" class="underline">+62 85156207150</a></li>
@@ -23,7 +23,7 @@
       </button>
     </nav>
     <main>
-        <img src="./asergei.png" alt="photo" class="absolute -top-24 h-[900px] w-[900px] object-contain mx-auto">
+        <img src="../assets/asergei.png" alt="photo" class="absolute -top-24 h-[900px] w-[900px] object-contain mx-auto">
       <section class="h-screen flex flex-col justify-center items-end">
         <div class="absolute title ml-auto mr-10 text-right text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold max-w-max text-color-one">
           <h1 class="mb-[1rem] text-ani" id="animated"> -Daffa Ahmad Ibrahim</h1>
