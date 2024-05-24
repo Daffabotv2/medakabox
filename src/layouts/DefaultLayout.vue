17afef48ff701aaa52f4dee80c387dd7b5799639
DAFFA AHMAD IBRAHIM <124247480+Daffabotv2@users.noreply.github.com>
2024-05-24 09:23:38 +0000

    ku pastikan

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,11 +4,13 @@
 </template>
 
 <script>
+import { defineComponent } from 'vue';
 import HomePage from '../components/HomePage.vue';
 
-export default {
-    components: {
-        HomePage
-    }
-}
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    HomePage,
+  },
+});
 </script>
