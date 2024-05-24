484580c2a3336c1b892cf8134dfe2005b2601f51
DAFFA AHMAD IBRAHIM <124247480+Daffabotv2@users.noreply.github.com>
2024-05-24 09:01:02 +0000

    eyyo

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '/src/views/Home.vue';
 import AboutPage from '/src/views/About.vue';
+
+const routes = [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/about',
+      name: 'AboutPage',
+      component: AboutPage
+    }
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes
+  });
+  
+  export default router;
