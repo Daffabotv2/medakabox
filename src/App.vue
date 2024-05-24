7b3ebeced041aacbc514ec044b67d5c374ab18d6
DAFFA AHMAD IBRAHIM <124247480+Daffabotv2@users.noreply.github.com>
2024-05-24 07:36:02 +0000

    ayuk

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view v-slot="{ Component }">
+      <component :is="Component" />
+    </router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    console.log('App mounted');
+  }
 };
 </script>
 
